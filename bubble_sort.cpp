399da73e717bcc1146c12b260cc41bdf18c91f6f
Tata-hub1011 <tanechka.101190@gmail.com>
2025-08-29 21:55:23 +0800

    change bubble_sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -19,3 +19,17 @@ void bubble_sort(std::vector<int>& arr)
             break;
     }
 }
+
+int main()
+{
+    std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+    bubble_sort(arr);
+    std::cout << "Sorted array is: ";
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+    return 0;
+}
